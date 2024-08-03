a0bfc903ad2aaa0a3e6b24341df7e810d553dd7d
md jobayer <saifullahisalm@gmail.com>
2024-08-03 22:01:46 +0600

    bd web

---
+++ apps.js
@@ -0,0 +1,50 @@
+let menuList = document.getElementById("menuList")
+menuList.style.maxHeight = "0px";
+
+function toggleMenu(){
+    if(menuList.style.maxHeight == "0px")
+    {
+        menuList.style.maxHeight = "300px";
+    }
+    else{
+        menuList.style.maxHeight = "0px";
+    }
+}
+
+        let menuIcon = document.querySelector('.menuIcon');
+        let nav = document.querySelector('.overlay-menu');
+
+        menuIcon.addEventListener('click', () => {
+            if (nav.style.transform != 'translateX(0%)') {
+                nav.style.transform = 'translateX(0%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            } else { 
+                nav.style.transform = 'translateX(-100%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            }
+        });
+
+        let toggleIcon = document.querySelector('.menuIcon');
+
+        toggleIcon.addEventListener('click', () => {
+            if (toggleIcon.className != 'menuIcon toggle') {
+                toggleIcon.className += ' toggle';
+            } else {
+                toggleIcon.className = 'menuIcon';
+            }
+        });
+
+ var copy = document.querySelector(".logos-slide").cloneNode(true);
+  document.querySelector(".logo-slider").appendChild(copy);   
+  
+  var count=0;
+  var stop=setInterval(pro,12);
+  function pro(){
+      count++
+      document.getElementById('num1').innerHTML=count;
+      if(count==200){
+          clearInterval(stop)
+      }
+  }
+  
+ 
